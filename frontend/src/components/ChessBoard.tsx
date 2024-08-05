15ecda56ae2478ed88fdfbb68cd75324f645cd51
Ragnar20-03 <roshanpp20@gmail.com>
2024-08-05 21:00:46 +0530

    ChessBoard Squares

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,5 +1,31 @@
-export const ChessBoard = () => {
-    return <div className="text-white-200 w-full">
-        ChessBoard
+import { Color, PieceSymbol, Square } from "chess.js";
+import { useState } from "react";
+
+export const ChessBoard = ({board , socket} : {
+    board : ({
+        square: Square;
+        type: PieceSymbol;
+        color: Color;
+    } | null)[][] ; 
+    socket : WebSocket
+}) => {
+
+    const [from , setFrom ] = useState<Square | null > (null )
+    const [to , setTo ] = useState<Square | null > (null )
+
+    return <div className="text-white-200 ">
+            {board.map((row , i ) => {
+                return <div key={i} className="flex">
+                    {row.map((square , j ) => {
+                        return <div key={j} className={`w-16 h-16 ${(i + j )% 2 == 0 ? 'bg-green-500' : "bg-white" }`}>
+                            <div className="w-full justify-center flex h-full ">
+                                <div className=" h-full justify-center flex flex-col"></div>
+                                {square ? square.type : "" }
+                            </div>
+                        </div>
+                    })}
+
+                </div>
+            })}
     </div>
 }
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -49,17 +49,19 @@ export const Game = () => {
     return <div className=" flex justify-center">
         <div className="pt-8 max-w-screen-lg w-full ">
             <div className="grid grid-cols-6 gap-4  w-full">
-                <div className=" col-span-4 bg-red-200 ">
-                    <ChessBoard />
+                <div className=" col-span-4   w-full flex justify-center ">
+                    <ChessBoard  socket={socket} board ={ board } />
                 </div>
-                <div className="col-span-2 bg-green-200 w-full">
-                    <Button onClick={ () => {
-                            socket.send(JSON.stringify({
-                                type : INIT_GAME
-                            }))
-                        }} >
-                        Play 
-                    </Button>
+                <div className="col-span-2 bg-slate-800 w-full flex justify-center">
+                    <div className="pt-8">
+                        <Button onClick={ () => {
+                                socket.send(JSON.stringify({
+                                    type : INIT_GAME
+                                }))
+                            }} >
+                            Play 
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
